365cd2ba54dddcdbed08f3c7ed71dc706993588e
Costa Ivo <ivo.costa@creativecapsule.com>
2025-02-15 07:56:09 +0530

    add base structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/quotes-app/',
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    host: '0.0.0.0'  // Allows access from other devices on the network
+  }
+});
